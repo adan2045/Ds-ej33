b1880cbcf34b23ad201a8d9ee6d1741692ef31d4
adan.gros <adan.gros@gmail.com>
2023-09-08 21:16:35 -0300

    ej33

--- acciones/Jugadores/consultarTodos.php
+++ acciones/Jugadores/consultarTodos.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'responses/consultarResponse.php';
+require_once 'responses/ConsultatodosResponse.php';
 require_once '../../modelo/club.php';
 require_once '../../modelo/jugador.php';
 
@@ -25,7 +25,7 @@ $j1->Id= '1';
 $j1->Nombre = 'Lionel Andrés Messi';
 $j1->Apodo= 'La Pulga,  El Mesías,  D10S';
 $j1->Altura = '1.70cm';
-$j1->Club= $c
+$j1->Club= $c;
 
 
 $j2=new Jugador();
@@ -33,15 +33,11 @@ $j2->Id= '2';
 $j2->Nombre = 'Lisandro Martínez';
 $j2->Apodo='Licha';
 $j2->Altura = '1.75cm';
-$j2->Club=$c2
+$j2->Club=$c2;
 
 
-
-
-
-
-$resp->$listJugador() = $j1;
-$resp->$listJugador() = $j2;
+$resp->ListJugadores[] = $j1;
+$resp->ListJugadores[] = $j2;
 
 
 echo json_encode($resp);
--- acciones/Jugadores/responses/ConsultatodosResponse.php
+++ acciones/Jugadores/responses/ConsultatodosResponse.php
@@ -1,7 +1,7 @@
 <?php
 class ConsultatodosResponse
 {
-    public $listJugador = array();
+    public $ListJugadores = array();
   
     
 }
